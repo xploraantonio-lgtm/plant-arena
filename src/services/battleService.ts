7edf30c4efac43efc049fe4b3cc9d90fae4a4850
xploraantonio-lgtm <xploraantonio@gmail.com>
2026-09-06 02:24:02 -0500

    refactor: add battle service facade

---
+++ src/services/battleService.ts
@@ -0,0 +1,4 @@
+import { SupabaseService } from './supabaseService'
+
+/** Fachada transicional para operaciones de batalla y resolución de partidas. */
+export const battleService = SupabaseService
